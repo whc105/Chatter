a8195b0f0cb25770178612cc20bbc11afbd57ae3
Wei Huan Chen <whc309@nyu.edu>
2018-04-08 18:19:01 +0000

    Refactored code

--- client/components/chat/chat.jsx
+++ client/components/chat/chat.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
+import CreateRoom from '../create-room/create-room.jsx';
 import './chat.css';
 
 export default class Chat extends React.Component {
@@ -27,57 +27,6 @@ export default class Chat extends React.Component {
 				<CreateRoom/>
 				<div id='search'>
 					<Search content={this.state.rooms}/>
-				</div>
-			</div>
-		);
-	}
-}
-
-class CreateRoom extends React.Component {
-	constructor(props) {
-		super(props);
-		this.createRoom = this.createRoom.bind(this);
-	}
-	
-	createRoom() {
-		const refs = this.refs;
-		axios.post('/chat/make-room', {
-			name: refs.rmName.value
-		}).then((res)=> {
-			if (true) {
-				window.location = '../chat';
-			}
-		});
-	}
-	
-	render() {
-		return(
-			<div id='rm-maker'>
-				<button type='button' data-toggle='modal' data-target='#create-room' className='bttn-slant bttn-md bttn-primary bttn-no-outline'>Create A Room</button>
-				<div className='modal animated zoomIn' id='create-room' tabIndex='-1' role='dialog' aria-labelledby='create-room-label' aria-hidden='true'>
-					<div className='modal-dialog' role='document'>
-						<div className='modal-content'>
-							<div className='modal-header'>
-								<h5 className='modal-title' id='create-room-label'>Create A Room</h5>
-							</div>
-							<div className='modal-body'>
-								<form className='form-group'>
-									<div className='input-group'>
-										<div className='input-group-prepend'>
-											<label className='input-group-text' htmlFor='input-group-rm-name'>
-												<i className='fas fa-warehouse'></i>
-											</label>
-										</div>
-										<input id='input-group-rm-name' type='text' placeholder='Room Name' ref='rmName'/>
-									</div>
-								</form>
-							</div>
-							<div className='modal-footer'>
-								<button type='button' className='btn btn-secondary' data-dismiss='modal'>Close</button>
-								<button onClick={this.createRoom} type='button' className='btn btn-primary'>Create Room</button>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 		);
