5ecaee5a020b776e4fd337d057f19528acaf619a
Wei Huan Chen <whc309@nyu.edu>
2018-04-12 22:16:07 +0000

    Added non-database chat

--- socketio.js
+++ socketio.js
@@ -1,14 +1,9 @@
 module.exports = io => {
-    io.on('connection', (socket)=> {
+    io.on('connection', socket=> {
         console.log('Connected');
         
         socket.on('disconnect', ()=> {
             console.log('Disconnected User');
-        });
-        
-        socket.on('change color', (color)=> {
-            console.log('Color changed', color);
-            io.sockets.emit('change color', color);
         });
         
         socket.on('send', (message)=> {
