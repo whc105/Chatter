99837eaba1745cabf9ad6ab7ca73779a112490cb
Wei Huan Chen <whc309@nyu.edu>
2018-04-21 17:38:56 +0000

    Connected chat search to chatroom without a redirect

--- client/components/search/room-search/room-search.jsx
+++ client/components/search/room-search/room-search.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import './room-search.css';
 
 export default class Search extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			rooms: [],
 			query: ''
 		};
 		this.liveSearch = this.liveSearch.bind(this);
-		this.dropdownRedirect = this.dropdownRedirect.bind(this);
 	}
 	
 	//Gets all the rooms and mounts it
-	componentWillMount() {
+	componentDidMount() {
 		axios.get('/api/getAllRooms')
 		.then(({data})=> {
 			this.setState({
@@ -30,31 +29,29 @@ export default class Search extends React.Component {
 		});
 	}
 	
-	//Redirect to the chatrooms
-	dropdownRedirect(roomID) {
-		window.location = `../chat/${roomID}`;
-	}
 	
 	mapToDropdown() {
 		const rooms = this.state.rooms;
 		const query = this.state.query.toLowerCase();
-		if (query === '') {
-			return;
-		}
 		const filterRooms = rooms.filter((room)=> {
 			return room.name.toLowerCase().indexOf(query) >= 0;
 		});
 		const mapToComponent = filterRooms.slice(0, 6).map((room)=> {
 			return (
-				<li onClick={()=> this.dropdownRedirect(room.id)} className='list-group-item bttn-minimal bttn-md bttn-default' key={room.id}>Name: {room.name}
-				<span className='room-id'>ID: {room.id}</span>
-				</li>);
+				<Link to={`/chat/${room.id}`} key={room.id}>
+					<button className='list-group-item bttn-minimal bttn-md bttn-default link-button'>
+						<span className='room-name'>Name: {room.name}</span>
+						<span className='room-id'>ID: {room.id}</span>
+					</button>
+				</Link>
+				);
 		});
 		return mapToComponent;
 	}
 	
 	render() {
-		const mapToDropdown = this.mapToDropdown();
+		//If query isn't blank, the results will pop up
+		const mapToDropdown = (this.state.query !== '') ? this.mapToDropdown() : undefined;
 		return(
 			<div id='search'>
 				<ul className='list-group'>
